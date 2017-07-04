e406c9992d209aa520510a679a88a8a8466ee537
v.goldyuk <kuzzyatina@gmail.com>
2017-07-04 14:42:22 +0300

    git init

---
+++ DedKorchma/DedKorchma/App_Start/BundleConfig.cs
@@ -0,0 +1,33 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace DedKorchma
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap.js",
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/style").Include(
+                "~/Content/css/bootstrap.min.css",
+                "~/Content/css/fonts.css",
+                "~/Content/css/style.css",
+                "~/Content/css/media.css"));
+        }
+    }
+}
